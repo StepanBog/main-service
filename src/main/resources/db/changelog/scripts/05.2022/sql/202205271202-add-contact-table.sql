188eae6991e0d8e0ff18089e9028be3980ebc6bd
stepa <SABogdanov@inno.tech>
2022-06-18 12:18:10 +0300

    init

--- src/main/resources/db/changelog/scripts/05.2022/sql/202205271202-add-contact-table.sql
+++ src/main/resources/db/changelog/scripts/05.2022/sql/202205271202-add-contact-table.sql
@@ -26,6 +26,5 @@ alter table employer
 
 
 insert into contact(id, employer_id, position, name, phone, email)
-VALUES ('fb56ac96-de5c-11ec-9d64-0242ac120002','7afcccda-dcf7-11ec-9d64-0242ac120002','MANAGER','Иванов Александр Иванович','89998887766','manager@email.com'),
-       ('956cd418-de5d-11ec-9d64-0242ac120002','7afcccda-dcf7-11ec-9d64-0242ac120002','EMPLOYERS_CONTACT','Матвеев Игорь Александрович','89995554758','contact2@email.com'),
-       ('8909e0b2-de5d-11ec-9d64-0242ac120002','7afcccda-dcf7-11ec-9d64-0242ac120002','EMPLOYERS_CONTACT','Александров Олег Игоревич','88884441122','contact1@email.com')
+VALUES ('fb56ac96-de5c-11ec-9d64-0242ac120002','7afcccda-dcf7-11ec-9d64-0242ac120002','MANAGER','Test Test Test','89998887766','manager@email.com'),
+       ('956cd418-de5d-11ec-9d64-0242ac120002','7afcccda-dcf7-11ec-9d64-0242ac120002','EMPLOYERS_CONTACT','Test2 Test2 Test2','89995554758','contact2@email.com');
